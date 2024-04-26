65be28d796a5e3a1affd48de4de7e5ee5a816fc6
KILLER42 <40739278+KILLER42@users.noreply.github.com>
2024-04-26 14:55:25 +0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const alphaArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+const alphaString = alphaArray.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+});
+
+console.log(alphaString);
